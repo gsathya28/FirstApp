5ef143abc2fe758472f071d8f8009bfa97686388
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2017-05-15 11:20:16 -0500

    Added Data Saving

--- app/src/main/java/com/clairvoyance/firstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/clairvoyance/firstapp/DisplayMessageActivity.java
@@ -1,5 +1,6 @@
 package com.clairvoyance.firstapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +8,16 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    String filename = "testFile";
+    FileOutputStream outputStream;
+    FileInputStream inputStream;
+    byte[] byteHolder;
+    String awesome = new String();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +29,25 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_APPEND);
+            outputStream.write(message.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            inputStream = openFileInput(filename);
+            inputStream.read(byteHolder, 0, 0);
+            awesome = byteHolder.toString();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
         TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(message);
+        textView.setText(awesome);
     }
 
     public void nextView(View view)
